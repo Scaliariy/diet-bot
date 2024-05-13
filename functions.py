4675c11a145f99673385cf948899d3d061127672
Nicol <scaliariy1@gmail.com>
2024-05-13 12:24:00 +0300

    added exceptions, changed some prompts, added disclaimer

--- functions.py
+++ functions.py
@@ -12,23 +12,15 @@ import streamlit as st
 
 # Define your models
 class UserInfo(BaseModel):
-    health_problems: str = Field(
-        description="If included in the message: User's health problems and any information from the message.")
+    health_info: str = Field(description="If included in the message: User's any health information from the message.")
     allergies: str = Field(description="If included in the message: User's allergies.")
     intolerances: str = Field(description="If included in the message: User's food intolerances.")
-    exclude: str = Field(description="If included in the message: What to exclude from the diet.")
-    add: str = Field(description="If included in the message: What to add from the diet.")
-    calories: str = Field(default=2400,
-                          description="If included in the message: Any calorie information. Default is 2400.")
-    type_of_diet: str = Field(description="If included in the message: Any information about the desired type of diet.")
-    meals_number: str = Field(default=3,
-                              description="If included in the message: Any information number of meals per day. Default is 3.")
-    other_restrictions: str = Field(
-        description="If included in the message: Any information about religious, cultural, or ethical dietary restrictions.")
+    exclude: str = Field(description="If included in the message: Which foods to exclude from the diet.")
+    add: str = Field(description="If included in the message: Which foods to add to your diet.")
 
 
 class Indication(BaseModel):
-    indeication_index: int = Field(description="Index of similar indications.")
+    indication_index: int = Field(description="Index of similar indications.")
     explanation: str = Field(description="Why you think so?")
 
 
@@ -64,8 +56,16 @@ def get_model(api_key, model) -> ChatOpenAI:
 
 def get_user_info(user_message: str, model: ChatOpenAI) -> UserInfo:
     query = (
-        f"Extract information about the user's health problems, his allergies, intolerances, "
-        f"wishes what exclude, wishes what add, calorie information, type of diet and other_restrictions from the message: {user_message}."
+        "You are a specialist in processing requests from a user who describes information about himself for the "
+        "purpose of assigning him a therapeutic diet."
+        "But there are cases when the user's message is incorrect or contains false information, so first check "
+        "whether the user's message is an adequate description of information about: "
+        "the user's health problems, his allergies, intolerances, "
+        "wishes, what to exclude and what to add, type of diet and other restrictions. "
+        "If the user's message is inadequate, contains many grammatical, lexical, "
+        "semantic errors, errors in medical terms, non-existent medical terms or invented made-up diseases, "
+        "humorous diseases then fill all fields as None."
+        f"If the user's message is correct, extract the relevant information from the message. User message: '{user_message}'"
     )
     parser = JsonOutputParser(pydantic_object=UserInfo)
     prompt = PromptTemplate(
@@ -80,14 +80,21 @@ def get_user_info(user_message: str, model: ChatOpenAI) -> UserInfo:
     except OutputParserException as e:
         fix_parser = OutputFixingParser.from_llm(parser=parser, llm=model)
         parsed_output = fix_parser.parse(output.content)
+
+    # Handling an invalid user request
+    if parsed_output['health_info'] == 'None' or parsed_output['health_info'] is None:
+        raise Exception("❗❗❗ Sorry, please write your request again. Please do it right ❗❗❗")
+
     return parsed_output
 
 
 def get_indication_info(user_info: UserInfo, indications_list: List[tuple], model: ChatOpenAI) -> Indication:
     query = (
         f"'List of medical indications': {indications_list}. "
-        f"Please write the index of the medical indication that corresponds to the information about the health of the user: '{user_info['health_problems']}'. "
-        "If user_info contains rubbish not related to medicine or you did not find an exact match in the 'List of medical indications', then indicate the index 99."
+        "You are a specialist in determining the appropriateness of user information and available medical indications."
+        f"Write the diet number that best describes and corresponds to the user’s health indications, keep in mind "
+        f"that some lines are similar, but you should choose the one that is as similar in meaning as possible: user_info ='{user_info['health_info']}'. "
+        "If you did not find an exact match in the 'List of medical indications', then return indication_index None."
     )
     parser = JsonOutputParser(pydantic_object=Indication)
     prompt = PromptTemplate(
@@ -114,8 +121,13 @@ def get_diet_options(temp: Dict, user_info: UserInfo, model: ChatOpenAI) -> Dail
         f"create for me a list of {3} balanced daily menu options. "
         f"Each daily menu on your list should consist of {5} meals."
         f"Each daily menu dish on your list should not contain products that may cause allergies: '{user_info['allergies']}' and intolerances: '{user_info['intolerances']}'."
-        "User is currently located in the country Ukraine. Please indicate prices for food products in UAH."
+        f"User is currently located in the country {'Ukraine'}. Please indicate prices for food products in {'UAH'}."
     )
+
+    if user_info['exclude'] or user_info['add']:
+        query += (f"Please note as additional conditions from the client that the menu should not contain products "
+                  f"such as: {user_info['exclude']}, but on the contrary, be sure to add products: {user_info['add']}. ")
+
     parser = JsonOutputParser(pydantic_object=DailyMenuList)
     prompt = PromptTemplate(
         template="Answer the user query.\n{format_instructions}\n{query}\n",
@@ -159,12 +171,18 @@ def main(user_message, api_key, language):
     indications_list = [(i, diet['indications']) for i, diet in enumerate(diet_data_list_en)]
     indications_list.append((99, "If it doesn't exactly match any other."))
     indication_info = get_indication_info(user_info, indications_list, model_3_5)
-    indeication_index = int(indication_info['indeication_index'])
-    temp = diet_data_list[indeication_index].copy()
-    # temp.pop('diet_name')
-    temp.pop('indications')
-    temp.pop('purpose')
-    temp.pop('eating_regime')
+    indication_index = int(indication_info['indication_index'])
+    if indication_index is not None:
+        temp = diet_data_list_en[indication_index].copy()
+        if 'indications' in temp:
+            temp.pop('indications')
+        if 'purpose' in temp:
+            temp.pop('purpose')
+        if 'eating_regime' in temp:
+            temp.pop('eating_regime')
+    else:
+        temp = user_info.copy()
+        temp['diet_name'] = f"General diet for '{user_info['health_info']}'"
     diet_options = get_diet_options(temp, user_info, model_4)
     if language == "Ukrainian ***:red[beta]***":
         translated_diet_options = translate_dict(diet_options)
@@ -174,10 +192,13 @@ def main(user_message, api_key, language):
 
 
 if __name__ == "__main__":
-    # Hello, I have a cold, please help me create a dietary menu for me. Please note that I am allergic to tree nuts and peanuts. I also have lactose intolerance.
-    user_message = ("Hello, I have a cold, please help me create a dietary menu for me. "
-                    "Please note that I am allergic to tree nuts and peanuts. "
-                    "I also have lactose intolerance.")
+    # Positive example:
+    # Hi there! I've been diagnosed with esophagitis and gastritis. Can you assist me in creating a suitable diet plan? I'm allergic to peanuts and soy. Also, I don't like onions, but I do like apples, keep that in mind.
+    # Negative example:
+    # Hiya, I've been diagnosed with flibberjabberitis and gobbledygookitis. Can you help me create a diet plan? I'm allergic to marshmallows and moon cheese.
+    user_message = ("Hi there! I've been diagnosed with esophagitis and gastritis. Can you assist me in creating a "
+                    "suitable diet plan? I'm allergic to peanuts and soy. Also, I don't like onions, but I do like "
+                    "apples, keep that in mind.")
     # Call the main function
     user_info = main(user_message, st.secrets["OPENAI_API_KEY"])
     print(user_info)
--- main.py
+++ main.py
@@ -9,7 +9,18 @@ st.title('🍉🤖 Diet Bot')
 language = st.radio("Select the language of your request and response:", ["English", "Ukrainian ***:red[beta]***"])
 
 # Sidebar for OpenAI API key input
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+st.sidebar.write("**Disclaimer**⚠️ \n\n"
+                 "*This bot generates diets based "
+                 "on the information you provide. We are not professional "
+                 "dietitians or doctors, and we are not responsible for the "
+                 "accuracy or effectiveness of the diets provided. "
+                 "Users are solely responsible for their health and "
+                 "the decisions they make based on the bot's recommendations. "
+                 "If you have any doubts or questions about your diet or health, "
+                 "consult a qualified professional. By using our bot, you agree to these terms.*")
+disclaimer = st.sidebar.checkbox("I have read and agree to the terms of the disclaimer.")
+if disclaimer:
+    openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 # Initialization result
 if 'result' not in st.session_state:
@@ -18,7 +29,7 @@ if 'result' not in st.session_state:
 
 # Function to generate response
 def generate_response(input_text, api_key, language):
-    with st.spinner('Wait for it...'):
+    with st.spinner('Wait about 1.5 minutes...'):
         st.session_state['result'] = main(input_text, api_key, language)
 
 
@@ -35,35 +46,39 @@ with st.form('my_form'):
                         placeholder='Example: Hello, I have a cold, please help me create a dietary menu for me. Please note that I am allergic to tree nuts and peanuts. I also have lactose intolerance.')
     submitted = st.form_submit_button('Submit')
 
-    # Warning for missing OpenAI API key
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    try:
+        # Warning for missing OpenAI API key
+        if not openai_api_key.startswith('sk-'):
+            st.warning('Please enter your OpenAI API key!', icon='⚠')
 
-    # Process submitted form
-    if submitted and openai_api_key.startswith('sk-'):
-        if text:
-            try:
-                generate_response(text, openai_api_key, language)
-            except AuthenticationError:
-                st.error('Please enter correct OpenAI API key!', icon='⚠')
-        else:
-            st.info('Please enter your request.', icon='⚠')
+        # Process submitted form
+        if submitted and openai_api_key.startswith('sk-'):
+            if text:
+                try:
+                    generate_response(text, openai_api_key, language)
+                except AuthenticationError:
+                    st.error('Please enter correct OpenAI API key!', icon='⚠')
+            else:
+                st.info('Please enter your request.', icon='⚠')
+    except NameError:
+        st.error('Please read and accept the disclaimer.', icon='⚠')
 
 # Display result if available
 if st.session_state['result']:
+    st.write("**Created daily diet options:**")
     result = st.session_state['result']
     for i, menu in enumerate(result['menus']):
         # Create tab for each menu option
-        with st.expander(f"Menu {i + 1}"):
-            st.write(f"Total Price: {menu['total_price']} {menu['currency']}")
+        with st.expander(f"**Menu {i + 1}**"):
+            st.write(f"***:red[Total Price]: {menu['total_price']} {menu['currency']}***")
             for meal in menu['meals']:
-                st.subheader(f"Meal {meal['meal_number']}: {meal['description']}")
+                st.subheader(f"{meal['description']}")
                 for dish in meal['dishes']:
-                    st.write(f"Name: {dish['name']}")
-                    st.write(f"Ingredients: {', '.join(dish['ingredients'])}")
-                    st.write(f"Price: {dish['price']} {menu['currency']}")
-                    st.write(f"Cooking Instructions: {dish['cooking_instructions']}")
-            # Generate PDF button
+                    st.write(f"**:violet[Name]: {dish['name']}**")
+                    st.write(f"**:orange[Ingredients]:** {', '.join(dish['ingredients'])}")
+                    st.write(f"**:blue[Cooking Instructions]:** {dish['cooking_instructions']}")
+                    st.write(f"**:green[Price]:** {dish['price']} {menu['currency']}")
+                # Generate PDF button
             if st.button(f"Generate PDF for Menu {i + 1}", key=f"generate-pdf-menu_{i + 1}"):
                 with st.spinner('Generating PDF...'):
                     time.sleep(1)
@@ -73,4 +88,3 @@ if st.session_state['result']:
                     st.download_button(f"Download pdf for Menu {i + 1}", f, f"diet_plan_{i + 1}.pdf",
                                        key=f"download-pdf-menu_{i + 1}")
 
-    st.write(result)
